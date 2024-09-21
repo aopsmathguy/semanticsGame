e5740a519c26ac5be5a81ca64a5b4d7fe13182b9
matthew <matzhao@gmail.com>
2024-09-21 14:52:22 -0400

    mobile

--- frontend/src/components/RoomList/styles.jsx
+++ frontend/src/components/RoomList/styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 export const StyledRoomListWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100vh;
+    height: 100dvh;
     width: 100vw;
     align-items: center;
     justify-content: center;
