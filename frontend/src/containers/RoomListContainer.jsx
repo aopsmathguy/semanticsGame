ff8d12296d1bc865c6424286abad428610f3cbb1
matthew <matzhao@gmail.com>
2026-03-28 21:34:20 -0400

    roomlist update every 2 seconds

--- frontend/src/containers/RoomListContainer.jsx
+++ frontend/src/containers/RoomListContainer.jsx
@@ -1,5 +1,5 @@
 //game screen
-import React from "react";
+import { useEffect } from "react";
 import RoomList from "../components/RoomList/RoomList";
 
 import { useSelector } from "react-redux";
@@ -9,6 +9,13 @@ import { emit } from "../ws";
 
 function RoomListContainer() {
     const roomList = useSelector(selectRoomList);
+
+    useEffect(() => {
+        emit("room-list-request");
+        const interval = setInterval(() => emit("room-list-request"), 2000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <RoomList
             roomList={roomList}
